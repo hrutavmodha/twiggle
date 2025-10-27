c8b568b8910ab6495cb5964994860f8197e67efc
Hrutav Modha <modhahrutav@gmail.com>
2025-10-27 14:20:45 +0530

    Added react-app in playgroudn/client

---
+++ playground/client/src/App.tsx
@@ -0,0 +1,16 @@
+export default function App() {
+    return (
+        <div>
+            <div>
+                <textarea></textarea>
+            </div>
+            <button
+                onClick={() => {
+                    alert('Hello World')
+                }}
+            >
+                Run
+            </button>
+        </div>
+    )
+}
