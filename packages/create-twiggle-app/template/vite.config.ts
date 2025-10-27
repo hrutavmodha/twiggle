24c27aa3b5b3aba9d2119c2c92d32c6ca951cee1
Hrutav Modha <modhahrutav@gmail.com>
2025-10-27 14:02:12 +0530

    Fixed errors in template's vite-config

--- packages/create-twiggle-app/template/vite.config.ts
+++ packages/create-twiggle-app/template/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import twiggle from 'vite-plugin-twiggle'
+import { twiggle } from 'vite-plugin-twiggle'
 
 export default defineConfig({
     plugins: [twiggle()],
--- packages/create-twiggle-app/tests/create-twiggle-app.test.ts
+++
@@ -1,5 +0,0 @@
-import { describe, expect } from 'vitest'
-import { createApp } from '../src/create-app'
-describe('create-twiggle-app', () => {
-    expect(createApp('twiggle-test', '.', '../template'))
-})
