c0ed8ec02769fe0f4f39fe1c0f516312caaf7320
Hrutav Modha <modhahrutav@gmail.com>
2025-10-30 15:14:39 +0530

    Removed playground temply

--- playground/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from 'twiggle/client'
-import App from './App'
-import './styles.css'
-
-const rootElement = document.getElementById('root') as HTMLDivElement
-
-render(<App />, rootElement)
