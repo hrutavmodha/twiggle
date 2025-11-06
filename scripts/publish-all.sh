fe7d63484a5dc410d21a546f9cf95ded8a1e5872
Hrutav Modha <modhahrutav@gmail.com>
2025-11-06 16:41:40 +0530

    Added publishing script

---
+++ scripts/publish-all.sh
@@ -0,0 +1,31 @@
+cd ../packages/babel-plugin-twiggle
+npm publish
+echo "Published babel-plugin-twiggle"
+
+cd ../create-twiggle-app
+npm publish
+echo "Published create-twiggle-app"
+
+cd ../esbuild-plugin-twiggle
+npm publish
+echo "Published esbuild-plugin-twiggle"
+
+cd ../parcel-plugin-twiggle
+npm publish
+echo "Published parcel-plugin-twiggle"
+
+cd ../rollup-plugin-twiggle
+npm publish
+echo "Published rollup-plugin-twiggle"
+
+cd ../twiggle
+npm publish
+echo "Published twiggle"
+
+cd ../vite-plugin-twiggle
+npm publish
+echo "Published vite-plugin-twiggle"
+
+cd ../webpack-plugin-twiggle
+npm publish
+echo "Published webpack-plugin-twiggle"
